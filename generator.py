e6666d99ea33879188b57d23ad8fd07048920afa
Nathan Bollig <nbollig@wisc.edu>
2020-10-22 12:21:39 -0500

    Experiment 'test'

---
+++ generator.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 09:32:57 2020
+
+@author: NBOLLIG
+"""
+import numpy as np
+import random
+
+class MarkovChain(object):
+    def __init__(self, transition_matrix, states):
+        """
+        Initialize the MarkovChain instance.
+ 
+        Parameters
+        ----------
+        transition_matrix: 2-D array
+            A 2-D array representing the probabilities of change of 
+            state in the Markov Chain.
+ 
+        states: 1-D array 
+            An array representing the states of the Markov Chain. It
+            needs to be in the same order as transition_matrix.
+        """
+        self.transition_matrix = np.atleast_2d(transition_matrix)
+        self.states = states
+        self.index_dict = {self.states[index]: index for index in 
+                           range(len(self.states))}
+        self.state_dict = {index: self.states[index] for index in
+                           range(len(self.states))}
+ 
+    def next_state(self, current_state):
+        """
+        Returns the state of the random variable at the next time 
+        instance.
+ 
+        Parameters
+        ----------
+        current_state: str
+            The current state of the system.
+        """
+        return np.random.choice(
+         self.states, 
+         p=self.transition_matrix[self.index_dict[current_state], :]
+        )
+ 
+    def generate_states(self, current_state, no=10):
+        """
+        Generates the next states of the system.
+ 
+        Parameters
+        ----------
+        current_state: str
+            The state of the current random variable.
+ 
+        no: int
+            The number of future states to generate.
+        """
+        future_states = []
+        for i in range(no):
+            next_state = self.next_state(current_state)
+            future_states.append(next_state)
+            current_state = next_state
+        return future_states
+
+def generate(num_instances = 500, p = 0.5, seed0 = 0, seed1 = 1):
+    """
+    Generates list of sequences X and list of labels y.
+    
+    Parameters:
+        num_instances: the number of total instances to generate
+        p: positive (1) class prevalance
+        seed0: random seed for generating Markov transition matrix for class 0
+        seed1: random seed for generating Markov transition matrix for class 1
+    
+    Returns:
+        X - list of sequences of length num_instances
+        y - list of labels (0 or 1)
+
+    """
+    aa_list = list('ARNDCEQGHILKMFPSTWYV')
+    n = len(aa_list)
+    num1 = int(num_instances*p)
+    num0 = num_instances - num1
+    
+    # Set transition matrices
+    np.random.seed(seed0)
+    m0 = np.random.rand(n, n)
+    for i in range(m0.shape[0]):
+        m0[i,:] = m0[i,:] / np.sum(m0[i,:])
+    
+    np.random.seed(seed1)
+    m1 = np.random.rand(n, n)
+    m1 = m1 / np.sum(m1)
+    for i in range(m1.shape[0]):
+        m1[i,:] = m1[i,:] / np.sum(m1[i,:])
+    
+    # Create Markov Chain objects
+    chain0 = MarkovChain(transition_matrix = m0, states = aa_list)
+    chain1 = MarkovChain(transition_matrix = m1, states = aa_list)
+    
+    X0 = []
+    for i in range(num0):
+        x0 = chain0.generate_states(current_state = random.choice(aa_list), no=100)
+        X0.append(''.join(x0))
+    
+    X1 = []
+    for i in range(num1):
+        x1 = chain1.generate_states(current_state = random.choice(aa_list), no=100)
+        X1.append(''.join(x1))
+    
+    # Create X and y lists
+    X = X0 + X1
+    y = list(np.zeros(num0, dtype=int)) + list(np.ones(num1, dtype=int))
+    
+    # Shuffle data
+    data = list(zip(X, y))
+    random.shuffle(data)
+    X, y = zip(*data)
+    
+    # Convert y to an array
+    y = np.array(y, dtype=int)
+    
+    return X, y, aa_list, m0, m1
+    
+    
+    
+    
+    
+    
+    
---
+++ seq_model.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 10:08:01 2020
+
+@author: NBOLLIG
+"""
+from generator import generate
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+import pickle
+
+
+def encode_raw_sequences(X_raw, aa_vocab):
+    """
+    Parameters:
+        X _raw: list of sequence strings
+        aa_vocab: list of aa tokens
+    
+    Output:
+        List of sequences each encoded as an array of size (100, 20) since every 
+        sequence is of length 100 and there are 20 amino acids
+    """
+    # Encode each instance as a list of indices into aa vocabulary
+    aa_dict = {}
+    for i, aa in enumerate(aa_vocab):
+        aa_dict[aa] = i
+    
+    X_seq = []
+    for x in X_raw:
+        x = list(x)
+        x = [aa_dict[aa] for aa in x]
+        X_seq.append(x)
+
+    # Transform to one-hot encoding
+    X = to_categorical(X_seq)
+    
+    return X
+
+def create_LSTM(X_train, X_val, y_train, y_val):
+    # define the  model
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(100,20)))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    # Train model
+    model.fit(X_train, y_train, epochs=50, batch_size=64, verbose=1)
+    
+    # Evaluate on test set
+    _, accuracy = model.evaluate(X_val, y_val, verbose=0)
+    print('Accuracy: %.2f' % (accuracy*100))
+    
+    return model
+
+def big_bang(num_instances=5000, p=0.5, seed0 = 0, seed1 = 1):
+    """
+    Generates sequence data and trains a model.
+    
+    Parameters:
+        num_instances: the number of total instances to generate
+        p: positive (1) class prevalance
+    
+    
+    Returns:
+        model: trained Keras model
+        X: list [X_train, X_val, X_test]
+        y: list [y_train, y_val, y_test]
+    """
+    # Generate data
+    X_raw, y, aa_vocab, m0, m1 = generate(num_instances=num_instances, p=p, seed0=seed0, seed1=seed1)
+    
+    # Encode X
+    X = encode_raw_sequences(X_raw, aa_vocab)
+    
+    # Split into train, validation, test (80/10/10)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=1)
+    X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5, random_state=1)
+    
+    # Define model
+    model = create_LSTM(X_train, X_val, y_train, y_val)
+    
+    return model, [X_train, X_val, X_test], [y_train, y_val, y_test], m0, m1
+
+def main(name, num_instances=5000, p=0.5, seed0 = 0, seed1 = 1):
+    """
+    Parameters:
+        name: a string identifier for the experiment
+        others as in big_bang
+    """
+    
+    model, X_list, y_list, m0, m1 = big_bang(num_instances=num_instances, p=p, seed0 = seed0, seed1 = seed1)
+    model.save(name+'_keras_model')
+    with open(name+'_X_list.pkl', 'wb') as f:
+        pickle.dump(X_list, f)
+    with open(name+'_y_list.pkl', 'wb') as f:
+        pickle.dump(y_list, f)
+    with open(name+'_m0.pkl', 'wb') as f:
+        pickle.dump(m0, f)
+    with open(name+'_m1.pkl', 'wb') as f:
+        pickle.dump(m1, f)
+
+if __name__ == "__main__":
+    main('test')
